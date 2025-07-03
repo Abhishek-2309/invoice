4db9910796c464373462fc6e17fe6e6933d2d68e
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-03 18:46:32 +0530

    Update prompts.py

--- Invoice_App/app/prompts.py
+++ Invoice_App/app/prompts.py
@@ -1,7 +1,7 @@
 identify_prompt = """
-You are an expert table reader from HTML. You are given one or multiple HTML tables extracted from an invoice.
+You are an expert table reader. Read one or multiple of the following tables extracted from an invoice and given in markdown string format.
 If only a single table is given, asssume it to be the main table
-Else, Read every table and its corresponding html code, and find the main table along with the corresponding values in each row
+Else, Read every table and its corresponding data, and find the main table along with the corresponding values in each row
 Identify the table containing line items (such as product/service, quantity, price, etc.).
 Items should only contain the itemized rows. For other rows that are towards the footer of table and don't fit items, include in summary_row
 Return strictly a JSON output with these fields alone in this format:
@@ -23,8 +23,9 @@ Tables:
 
 
 kv_prompt = """
-You are given the body of an invoice (in HTML/Markdown) excluding the main line item table.
-
+You are given the body of an invoice (in Markdown) 
+[Main Table Start] and [Main Table End] mark the start and end of the main line table
+Read the entire markdown body and extract all the following details except the line items in the main line table.
 Extract all invoice metadata as structured key-value pairs. An example JSON schema is as follows:
 ~~~json
 {{
