4cc32ba1a8f3c330f903daf89fe9723582b12bdf
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-03 16:02:52 +0530

    Update ocr.py

--- Invoice_App/app/ocr.py
+++ Invoice_App/app/ocr.py
@@ -19,7 +19,7 @@ def strip_prompt_from_output(text: str) -> str:
     return text.strip()  # fallback: return everything
 def ocr_page_with_nanonets(image_path: str, max_new_tokens=15000) -> str:
     image = Image.open(image_path)
-    prompt = """Extract the text from the above document as if you were reading it naturally and translate to english for none english document. Return the tables in html format. Return the equations in LaTeX representation. If there is an image in the document and image caption is not present, add a small description of the image inside the <img></img> tag; otherwise, add the image caption inside <img></img>. Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY</watermark>. Page numbers should be wrapped in brackets. Ex: <page_number>14</page_number> or <page_number>9/22</page_number>. Prefer using ☐ and ☑ for check boxes."""
+    prompt = """Extract the text from the above document as if you were reading it naturally and translate to english for none english document. Return the tables in normal format inside <table></table> tags. Return the equations in LaTeX representation. If there is an image in the document and image caption is not present, add a small description of the image inside the <img></img> tag; otherwise, add the image caption inside <img></img>. Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY</watermark>. Page numbers should be wrapped in brackets. Ex: <page_number>14</page_number> or <page_number>9/22</page_number>. Prefer using ☐ and ☑ for check boxes."""
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": [{"type": "image", "image": f"file://{image_path}"}, {"type": "text", "text": prompt}]}
