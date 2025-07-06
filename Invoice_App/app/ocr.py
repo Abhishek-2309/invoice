9b21b376b1888d1bf4869d59fbe8529322a40c2c
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-07 01:32:48 +0530

    Update ocr.py

--- Invoice_App/app/ocr.py
+++ Invoice_App/app/ocr.py
@@ -1,4 +1,3 @@
-"""
 from PIL import Image
 from transformers import AutoModelForImageTextToText, AutoTokenizer, AutoProcessor
 import torch
@@ -18,6 +17,7 @@ def strip_prompt_from_output(text: str) -> str:
     if len(parts) == 2:
         return parts[1].strip()
     return text.strip()  # fallback: return everything
+    
 def ocr_page_with_nanonets(image_path: str, max_new_tokens=15000) -> str:
     image = Image.open(image_path)
     prompt = """Extract the text from the above document as if you were reading it naturally and translate to english for none english document. Return the tables in html format. Return the equations in LaTeX representation. If there is an image in the document and image caption is not present, add a small description of the image inside the <img></img> tag; otherwise, add the image caption inside <img></img>. Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY</watermark>. Page numbers should be wrapped in brackets. Ex: <page_number>14</page_number> or <page_number>9/22</page_number>. Prefer using ☐ and ☑ for check boxes."""
@@ -33,4 +33,3 @@ def ocr_page_with_nanonets(image_path: str, max_new_tokens=15000) -> str:
     outputs = ocr_model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=False)
     markdown = ocr_processor.batch_decode(outputs, skip_special_tokens=True)[0]
     return strip_prompt_from_output(markdown)
-"""
