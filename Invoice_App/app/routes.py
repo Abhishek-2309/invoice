da2b1241e5d023f7df162811dd7a98c8adf04340
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-21 18:08:19 +0530

    Update routes.py

--- Invoice_App/app/routes.py
+++ Invoice_App/app/routes.py
@@ -1,14 +1,20 @@
-import os, shutil
+import os
+import shutil
 from fastapi import APIRouter, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
+from app.llm_processing import process_invoice_dir
 from app.ocr import ocr_page_with_nanonets
-from app.llm_processing import process_invoice, llm
+from app.Folder_Processing import process_zip
+from typing import Dict
 
 UPLOAD_DIR = "uploads"
+JSON_OUTPUT_DIR = os.path.join(UPLOAD_DIR, "json_results")
+
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+os.makedirs(JSON_OUTPUT_DIR, exist_ok=True)
 
 router = APIRouter()
-    
+
 @router.post("/upload")
 async def upload_invoice(file: UploadFile = File(...)):
     suffix = os.path.splitext(file.filename)[-1].lower()
@@ -17,9 +23,17 @@ async def upload_invoice(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, f)
     try:
         markdown = ocr_page_with_nanonets(temp_path)
-        print('Router-markdown', markdown)
-        structured_json = process_invoice(markdown, llm)
-        print('Router-json', structured_json)
+        structured_json = process_invoice_dir(markdown)
         return structured_json
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Processing failed: {e}")
+
+@router.post("/upload_zip")
+async def upload_zip(file: UploadFile = File(...)) -> Dict[str, dict]:
+    if not file.filename.endswith(".zip"):
+        raise HTTPException(status_code=400, detail="Please upload a .zip file")
+    try:
+        result = process_zip(file, JSON_OUTPUT_DIR)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Bulk processing failed: {e}")
