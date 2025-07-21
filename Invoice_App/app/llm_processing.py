3c436813104d8064d94672d52bd6fb390b6b31f1
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-21 18:06:46 +0530

    Update llm_processing.py

--- Invoice_App/app/llm_processing.py
+++ Invoice_App/app/llm_processing.py
@@ -339,30 +339,28 @@ def process_invoice(markdown_html: str, tokenizer, model) -> dict:
     filled_prompt = kv2_prompt.replace("{doc_body}", markdown_html)
     #calling qwen
     messages = [{"role": "user", "content": filled_prompt}]
-    model_inputs = tokenizer.apply_chat_template(
-        messages,
-        tokenize=True,
-        add_generation_prompt=True,
-        enable_thinking=False,
-        return_tensors="pt"
+    input_ids = tokenizer.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    enable_thinking=False,
+    return_tensors="pt"
     )
-    
-    model_inputs = {k: v.to(model.device) for k, v in model_inputs.items()}
-    
+    model_inputs = {
+        "input_ids": input_ids.to(model.device)
+    }  
     generated_ids = model.generate(
         **model_inputs,
         max_new_tokens=2048,
-        do_sample=False,
-        temperature=0.0,
-        top_p=1.0,
-        top_k=50,
+        do_sample = False,
+        temperature = 0.0,
+        top_p = 1.0,
+        top_k = 50,
         repetition_penalty=1.1,
         use_cache=True
     )
-    
-    output_ids = generated_ids[0][model_inputs["input_ids"].shape[1]:]
+    output_ids = generated_ids[0][len(model_inputs["input_ids"][0]):]
     full_output = tokenizer.decode(output_ids, skip_special_tokens=True)
-
     print(full_output)
     fields_json = extract_json_from_output(full_output)
 
