3e6f8ff5c6e9efd52f17378d9178beafa01ac69e
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-22 15:33:12 +0530

    Update main.py

--- Invoice_App/app/main.py
+++ Invoice_App/app/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from app.routes import router
+from app.llm_engine import load_llm
 
 app = FastAPI()
-app.include_router(router)
 
+@app.on_event("startup")
+def startup_event():
+    print("Loading Qwen LLM...")
+    load_llm()
+    print("Qwen model loaded successfully.")
+
+app.include_router(router)
 @app.get("/")
 def root():
     return {"status": "FastAPI is running!"}
