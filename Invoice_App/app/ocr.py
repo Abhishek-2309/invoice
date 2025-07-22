6849639104f1ea785f2ef30c8f63c727dba6a1b2
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-22 16:29:52 +0530

    Update ocr.py

--- Invoice_App/app/ocr.py
+++ Invoice_App/app/ocr.py
@@ -28,4 +28,5 @@ def ocr_page_with_nanonets(image_path: str, max_new_tokens=4000) -> str:
     inputs = ocr_processor(text=[text], images=[image], return_tensors="pt", padding=True).to(ocr_model.device)
     outputs = ocr_model.generate(**inputs, max_new_tokens=max_new_tokens, do_sample=False)
     markdown = ocr_processor.batch_decode(outputs, skip_special_tokens=True)[0]
+    torch.cuda.empty_cache()
     return strip_prompt_from_output(markdown)
