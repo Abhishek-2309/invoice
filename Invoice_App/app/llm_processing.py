6bb062cb64eb363cef6a54b9d829963fb37bb167
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-22 10:44:32 +0530

    Update llm_processing.py

--- Invoice_App/app/llm_processing.py
+++ Invoice_App/app/llm_processing.py
@@ -25,22 +25,30 @@ def process_invoice_dir(markdown: str):
 def process_invoice(markdown_html: str, tokenizer, model) -> dict:
     filled_prompt = kv2_prompt.replace("{doc_body}", markdown_html)
     messages = [{"role": "user", "content": filled_prompt}]
-    input_ids = tokenizer.apply_chat_template(
+    input = tokenizer.apply_chat_template(
         messages,
         tokenize=True,
         add_generation_prompt=True,
-        return_tensors="pt"
+        return_tensors="pt",
+        padding=True,     
+        max_length = 8192
     ).to(model.device)
+    
+    input_ids = inputs["input_ids"]
+    attention_mask = inputs["attention_mask"]
 
     generated_ids = model.generate(
         input_ids=input_ids,
+        attention_mask = attention_mask,
         max_new_tokens=4096,
         do_sample=False,
         temperature=0.0,
         top_p=1.0,
-        repetition_penalty=1.1
+        top_k = 50
+        repetition_penalty=1.1,
+        use_cache = True
     )
-    output_ids = generated_ids[0][len(input_ids[0]):]
+    output_ids = generated_ids[0][input_ids.shape[1]:]
     full_output = tokenizer.decode(output_ids, skip_special_tokens=True)
     print(full_output)
     fields_json = extract_json_from_output(full_output)
