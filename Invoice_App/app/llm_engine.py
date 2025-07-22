0cff2ee9c6af1887948ed86c4e8c53e0f6f45b7e
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-22 10:41:46 +0530

    Update llm_engine.py

--- Invoice_App/app/llm_engine.py
+++ Invoice_App/app/llm_engine.py
@@ -13,4 +13,6 @@ def load_llm():
             load_in_4bit=True,
             device_map="auto"
         )
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
     return model, tokenizer
