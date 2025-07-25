735672da6edbe51f16af40fad3ba77000862c162
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-07-25 14:25:08 +0530

    Update llm_processing.py

--- Invoice_App/app/llm_processing.py
+++ Invoice_App/app/llm_processing.py
@@ -27,38 +27,43 @@ def process_invoice_dir(markdown: str):
 def process_invoice(markdown_html: str, tokenizer, model) -> dict:
     filled_prompt = kv2_prompt.replace("{doc_body}", markdown_html)
     messages = [{"role": "user", "content": filled_prompt}]
+    
     input_ids = tokenizer.apply_chat_template(
         messages,
         tokenize=True,
         add_generation_prompt=True,
-        enable_thinking = False,
+        enable_thinking=False,
         return_tensors="pt"
     )
+
     model_inputs = {
         "input_ids": input_ids.to(model.device)
-    }  
-	with torch.no_grad():
-	    generated_ids = model.generate(
-	        **model_inputs,
-	        max_new_tokens=4096,
-	        do_sample=False,
-	        temperature=0.7,
-	        top_p=0.8,
-	        top_k=20,
-	        use_cache=True
-	    )
-	
-	    output_ids = generated_ids[0][len(model_inputs["input_ids"][0]):]
-	    full_output = tokenizer.decode(output_ids, skip_special_tokens=True)
+    }
+
+    with torch.no_grad():
+        generated_ids = model.generate(
+            **model_inputs,
+            max_new_tokens=4096,
+            do_sample=False,
+            temperature=0.7,
+            top_p=0.8,
+            top_k=20,
+            use_cache=True
+        )
+
+        output_ids = generated_ids[0][len(model_inputs["input_ids"][0]):]
+        full_output = tokenizer.decode(output_ids, skip_special_tokens=True)
 
     torch.cuda.empty_cache()
     print(full_output)
+
     fields_json = extract_json_from_output(full_output)
     kv_result = KVResult(**fields_json)
+    
     return InvoiceSchema(
-	    Header=kv_result.Header,
-	    Main_Table=kv_result.Main_Table,
-	    Payment_Terms=kv_result.Payment_Terms,
-	    Summary=kv_result.Summary,
-	    Other_Important_Sections=kv_result.Other_Important_Sections,
-	).model_dump()
+        Header=kv_result.Header,
+        Main_Table=kv_result.Main_Table,
+        Payment_Terms=kv_result.Payment_Terms,
+        Summary=kv_result.Summary,
+        Other_Important_Sections=kv_result.Other_Important_Sections,
+    ).model_dump()
