b629523ae452908b89662305fc80cd621eaf22f5
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-08-06 10:56:11 +0530

    Update prompts.py

--- Invoice_App/app/prompts.py
+++ Invoice_App/app/prompts.py
@@ -1,4 +1,9 @@
 kv2_prompt = """
+What is Invoice no? Markdown:
+{doc_body}
+""""
+
+kv_prompt = """
   You are given the markdown of an Invoice document with tables enclosed within '<table>' tags.
   Map all the fields in the below JSON schema to the closest value associated with it that is present in the markdown input.
   Note:
