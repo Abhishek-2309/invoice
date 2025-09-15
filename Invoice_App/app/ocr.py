927ee939ef59a34b6a8da572e7e3be3d94b0e03a
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-09-15 13:33:58 +0530

    Update ocr.py

--- Invoice_App/app/ocr.py
+++ Invoice_App/app/ocr.py
@@ -1,22 +1,10 @@
 from PIL import Image
 from transformers import AutoModelForImageTextToText, AutoTokenizer, AutoProcessor
-#from transformers import BitsAndBytesConfig
 import torch
 import re
 
 ocr_model_id = "nanonets/Nanonets-OCR-s"
-"""
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16,  # L4 supports float16 natively
-    bnb_4bit_use_double_quant=True
-)
-ocr_model = AutoModelForImageTextToText.from_pretrained(
-    ocr_model_id,
-    quantization_config=bnb_config,
-    device_map="auto"
-).eval()
-"""
+
 ocr_model = AutoModelForImageTextToText.from_pretrained(
     ocr_model_id, torch_dtype="auto", device_map="auto"
 ).eval()
