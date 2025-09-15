ba7a914ae6ae9784172e249d0463781f04b56bca
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-09-15 18:49:02 +0530

    Update ocr.py

--- Invoice_App/app/ocr.py
+++ Invoice_App/app/ocr.py
@@ -17,13 +17,11 @@ def strip_prompt_from_output(text: str) -> str:
     split_pattern = r"(?:^|\n)assistant\s*\n"
     parts = re.split(split_pattern, text, maxsplit=1)
     if len(parts) == 2:
-        print(parts)
         return parts[1].strip()
     return text.strip()
 
 def ocr_page_with_nanonets(image_path: str, max_new_tokens=4000) -> str:
     image = Image.open(image_path)
-    #prompt = "Extract the text from the above document as if you were reading it naturally. Return the tables in html format."
     prompt2 = """Extract the text from the above document as if you were reading it naturally. Return the tables in html format. Return the equations in LaTeX representation. If there is an image in the document and image caption is not present, add a small description of the image inside the <img></img> tag; otherwise, add the image caption inside <img></img>. Watermarks should be wrapped in brackets. Ex: <watermark>OFFICIAL COPY</watermark>. Page numbers should be wrapped in brackets. Ex: <page_number>14</page_number> or <page_number>9/22</page_number>. Prefer using ☐ and ☑ for check boxes."""
     messages = [
         {"role": "system", "content": "You are a helpful assistant."},
