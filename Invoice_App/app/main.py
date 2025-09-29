bc0b1cb6693b501ee5a344f126b860410abc2a42
Abhishek K.R <122096235+Abhishek-2309@users.noreply.github.com>
2025-09-29 08:10:29 +0530

    Update main.py

--- Invoice_App/app/main.py
+++ Invoice_App/app/main.py
@@ -1,19 +1,11 @@
 import os
-import unsloth
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
 from fastapi import FastAPI
 from app.routes import router
-from app.llm_engine import load_llm
 
-app = FastAPI()
+app = FastAPI(title="Invoice")
 
-@app.on_event("startup")
-def startup_event():
-    print("Loading Qwen LLM...")
-    load_llm()
-    print("Qwen model loaded successfully.")
-
-app.include_router(router)
 @app.get("/")
 def root():
     return {"status": "FastAPI is running!"}
+
+app.include_router(router)
